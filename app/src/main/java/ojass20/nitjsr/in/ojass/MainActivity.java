4a52ac04d5e65a08811e607a572417f2890e6b52
naman7kr <naman7kr@gmail.com>
2019-09-23 20:35:51 +0530

    First commit

---
+++ app/src/main/java/ojass20/nitjsr/in/ojass/MainActivity.java
@@ -0,0 +1,13 @@
+package ojass20.nitjsr.in.ojass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
