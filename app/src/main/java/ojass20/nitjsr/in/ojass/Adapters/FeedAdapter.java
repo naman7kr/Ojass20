57bcb4134e17b0f83ef515f7122e6305c37ba5a0
tachodril <rajlevel97@gmai.com>
2019-11-23 11:49:52 +0530

    like implementation

--- app/src/main/java/ojass20/nitjsr/in/ojass/Adapters/FeedAdapter.java
+++ app/src/main/java/ojass20/nitjsr/in/ojass/Adapters/FeedAdapter.java
@@ -23,88 +23,142 @@ import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import ojass20.nitjsr.in.ojass.Models.FeedPost;
+import ojass20.nitjsr.in.ojass.Models.Likes;
 import ojass20.nitjsr.in.ojass.R;
 
-public class FeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.CustomViewHolder> {
 
     private Context context;
     private ArrayList<FeedPost> feedPosts;
+    private String mcurrentuid;
+    private boolean is_already_liked=false;
+    private String mpost_id="";
 
     public static class CustomViewHolder extends RecyclerView.ViewHolder {
         public LinearLayout feedLayout;
-        public TextView subevent_name;
-
+        public TextView subevent_name,like_text,eventname,content;
+        public ImageView like_icon,postImage;
+        public LinearLayout like_layout;
+        RelativeLayout postImageView;
+        ProgressBar progressBar;
         public CustomViewHolder(@NonNull View itemView) {
             super(itemView);
             feedLayout = (LinearLayout) itemView;
+            progressBar=itemView.findViewById(R.id.feed_post_image_progress_bar);
+            postImageView=itemView.findViewById(R.id.feed_post_image_parent);
+            postImage=itemView.findViewById(R.id.feed_post_image);
             subevent_name=itemView.findViewById(R.id.feed_post_sub_event_name);
+            eventname=itemView.findViewById(R.id.feed_post_event_name);
+            content=itemView.findViewById(R.id.feed_post_content);
+            like_text=itemView.findViewById(R.id.like_textview);
+            like_icon=itemView.findViewById(R.id.like_icon);
+            like_layout=itemView.findViewById(R.id.feed_post_upvote);
         }
     }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LinearLayout feedLayout = (LinearLayout) (LayoutInflater.from(context).inflate(
                 R.layout.feed_item, parent, false));
         return new CustomViewHolder(feedLayout);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        CustomViewHolder customViewHolder = (CustomViewHolder) holder;
-        ((TextView) (customViewHolder.feedLayout.findViewById(R.id.feed_post_event_name))).setText(
-                feedPosts.get(position).getEvent()
-        );
-
-        //Log.e("HEEE", " event = "+feedPosts.get(position).getEvent() );
-        //Log.e("HEEE", " subevent = "+feedPosts.get(position).getSubEvent() );
-
-        ((TextView) (customViewHolder.feedLayout.findViewById(R.id.feed_post_sub_event_name))).setText(
-                feedPosts.get(position).getSubEvent()
-        );
-
-        //((CustomViewHolder) holder).subevent_name.setText(feedPosts.get(position).getSubEvent());
-        //Toast.makeText(context, "subevent name "+feedPosts.get(position).getEvent(), Toast.LENGTH_SHORT).show();
-
-        ((TextView) (customViewHolder.feedLayout.findViewById(R.id.feed_post_content))).setText(
-                feedPosts.get(position).getContent()
-        );
-
-        RelativeLayout postImageView = (RelativeLayout) (customViewHolder.feedLayout.findViewById(
-                R.id.feed_post_image_parent
-        ));
-        postImageView.setVisibility(View.VISIBLE);
-
-        final ProgressBar progressBar = (ProgressBar) (customViewHolder.feedLayout.findViewById(
-                R.id.feed_post_image_progress_bar
-        ));
-        progressBar.setVisibility(View.VISIBLE);
-
-        final ImageView postImage = (ImageView) (customViewHolder.feedLayout.findViewById(R.id.feed_post_image));
+    public void onBindViewHolder(@NonNull final CustomViewHolder holder, final int position) {
+        holder.eventname.setText(feedPosts.get(position).getEvent());
+        holder.subevent_name.setText(feedPosts.get(position).getSubEvent());
+        holder.content.setText(feedPosts.get(position).getContent());
+        holder.like_text.setText(feedPosts.get(position).getLikes().size()+" Likes");
+        holder.postImageView.setVisibility(View.VISIBLE);
+        holder.progressBar.setVisibility(View.VISIBLE);
         Log.e("Hey", feedPosts.get(position).getImageURL());
         if (feedPosts.get(position).getImageURL() == null) {
-            postImageView.setVisibility(View.GONE);
+            holder.postImageView.setVisibility(View.GONE);
         } else {
             Glide.with(context)
                     .load(feedPosts.get(position).getImageURL())
                     .listener(new RequestListener<Drawable>() {
                         @Override
                         public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            holder.progressBar.setVisibility(View.GONE);
                             return false;
                         }
 
                         @Override
                         public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-                            progressBar.setVisibility(View.GONE);
+                            holder.progressBar.setVisibility(View.GONE);
                             return false;
                         }
                     })
-                    .into(postImage);
+                    .into(holder.postImage);
         }
+        mpost_id=feedPosts.get(position).getPostid();
+
+//        ArrayList<Likes> mlikes=feedPosts.get(position).getLikes();
+//        for(Likes like : mlikes) {
+//            if (like.getUser_id().equals(mcurrentuid)) {
+//                is_already_liked = true;
+//                holder.like_icon.setImageResource(R.drawable.upvoted);
+//                break;
+//            } else
+//                is_already_liked = false;
+//        }
+
+        is_already_liked=feedPosts.get(position).isIs_already_liked();
+        if(is_already_liked==true){
+            holder.like_icon.setImageResource(R.drawable.upvoted);
+        }
+
+        Log.i( "onBindViewHolder: ",is_already_liked+"");
+
+
+
+        final DatabaseReference dref= FirebaseDatabase.getInstance().getReference().child("Feeds");
+
+
+        holder.like_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.e("TAG", "onClick: "+feedPosts.get(position).getPostid());
+
+                //HashMap<String,Object> hs=new HashMap<>();
+                HashMap<String,Object> nc=new HashMap<>();
+                if(is_already_liked){
+//                    count--;
+//                    hs.put("likescount",Integer.toString(count));
+//                    dref.child(mpost_id).updateChildren(hs);
+                    Log.e("TAG", "onClick: level 2");
+                    nc.put(mcurrentuid,null);
+                    dref.child(feedPosts.get(position).getPostid()).child("likes").child(mcurrentuid).setValue(null);
+
+
+
+                }
+                else{
+                    Log.e("TAG", "onClick: level 6");
+
+                    //nc.put("83","");
+                    dref.child(feedPosts.get(position).getPostid()).child("likes").child(mcurrentuid).setValue(mcurrentuid);
+                }
+
+            }
+        });
 
     }
 
@@ -114,10 +168,11 @@ public class FeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         return feedPosts.size();
     }
 
-    public FeedAdapter(Context context, ArrayList<FeedPost> mfeedPosts) {
+    public FeedAdapter(Context context, ArrayList<FeedPost> mfeedPosts,String currentuid) {
         this.context = context;
-        feedPosts = new ArrayList<>();
+        //feedPosts = new ArrayList<>();
         this.feedPosts = mfeedPosts;
+        this.mcurrentuid=currentuid;
         Log.e("VIVZ", "FeedAdapter: called COUNT = " + mfeedPosts.size());
     }
 }
