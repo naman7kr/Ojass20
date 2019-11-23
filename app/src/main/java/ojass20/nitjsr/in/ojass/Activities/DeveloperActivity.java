ce12d70bd25e09f1d294acf18c3c7eff5920bc0e
Abhinav Mani <abhinav1946@gmail.com>
2019-11-24 00:54:28 +0530

    SelectDrawerItem Set

---
+++ app/src/main/java/ojass20/nitjsr/in/ojass/Activities/DeveloperActivity.java
@@ -0,0 +1,16 @@
+package ojass20.nitjsr.in.ojass.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ojass20.nitjsr.in.ojass.R;
+
+public class DeveloperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developer);
+    }
+}
