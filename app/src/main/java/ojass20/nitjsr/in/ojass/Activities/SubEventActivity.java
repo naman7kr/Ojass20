b9a7e636c356c452517ddeffacb0285bd2449fdd
Ayush Dubey <2017UGCS079@nitjsr.ac.in>
2020-01-08 00:23:11 +0530

    Add Fab and its menus

--- app/src/main/java/ojass20/nitjsr/in/ojass/Activities/SubEventActivity.java
+++ app/src/main/java/ojass20/nitjsr/in/ojass/Activities/SubEventActivity.java
@@ -2,26 +2,43 @@ package ojass20.nitjsr.in.ojass.Activities;
 
 import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.palette.graphics.Palette;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import ojass20.nitjsr.in.ojass.Adapters.SubEventsAdapter;
 import ojass20.nitjsr.in.ojass.Fragments.EventBottomSheet;
 import ojass20.nitjsr.in.ojass.Models.SubEventsModel;
@@ -30,23 +47,35 @@ import ojass20.nitjsr.in.ojass.Utils.Constants;
 import ojass20.nitjsr.in.ojass.Utils.RecyclerClickInterface;
 
 public class SubEventActivity extends AppCompatActivity {
+    private LinearLayout mAboutLayout, mHeadLayout;
     private RecyclerView rView;
-    private ArrayList<SubEventsModel> data=new ArrayList<>();
+    private ArrayList<SubEventsModel> data = new ArrayList<>();
     private int mainEventPosition;
     private ImageView iv;
+    private FloatingActionButton mFab;
     private FrameLayout frag_frame;
-    private static boolean bottomSheetOpen=false;
+    private static boolean bottomSheetOpen = false;
     private static int toolbarIconColor = Color.BLACK;
+    private Animation fadeIn, fadeOut;
     Toolbar toolbar;
     public static int position;
     public static String actionBarTitle;
+    private boolean isOpen;
+    private ArrayList<LinearLayout> mLL;
+    private ArrayList<ImageView> mProfile;
+    private ArrayList<ImageView> mWhatsapp;
+    private ArrayList<ImageView> mCall;
+    private TextView mAbout, mHeading;
+    private View mDivider1, mDivider2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_event);
         init();
         //get intent get main event
-        mainEventPosition = getIntent().getIntExtra("event_id",0); //from intent
+        mainEventPosition = getIntent().getIntExtra("event_id", 0); //from intent
 
 
         rView.setLayoutManager(new LinearLayoutManager(this));
@@ -63,20 +92,118 @@ public class SubEventActivity extends AppCompatActivity {
                 getPostion(position);
             }
         };
-        rView.setAdapter(new SubEventsAdapter(this,getData(),mInterface));
+        rView.setAdapter(new SubEventsAdapter(this, getData(), mInterface));
         //set Image
         iv.setImageResource(Constants.eventImg[mainEventPosition]);
 
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isOpen) {
+                    mHeadLayout.startAnimation(fadeOut);
+                    mAboutLayout.startAnimation(fadeOut);
+                    mHeadLayout.setVisibility(View.INVISIBLE);
+                    mAboutLayout.setVisibility(View.INVISIBLE);
+                    mHeadLayout.setClickable(false);
+                    mAboutLayout.setClickable(false);
+                    isOpen = false;
+                    mFab.setImageDrawable(getDrawable(R.drawable.ic_keyboard_arrow_up_black_24dp));
+                } else {
+                    mHeadLayout.startAnimation(fadeIn);
+                    mAboutLayout.startAnimation(fadeIn);
+                    mHeadLayout.setVisibility(View.VISIBLE);
+                    mAboutLayout.setVisibility(View.VISIBLE);
+                    mHeadLayout.setClickable(true);
+                    mAboutLayout.setClickable(true);
+                    isOpen = true;
+                    mFab.setImageDrawable(getDrawable(R.drawable.ic_keyboard_arrow_down_black_24dp));
+                    TranslateAnimation tr = new TranslateAnimation(0.0f, 0.0f, 0, 30);
+                    tr.setDuration(100);
+                    mFab.startAnimation(tr);
+                }
+            }
+        });
+
+        mHeadLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Rect displayRectangle = new Rect();
+                Window window = SubEventActivity.this.getWindow();
+                window.getDecorView().getWindowVisibleDisplayFrame(displayRectangle);
+                final AlertDialog.Builder builder = new AlertDialog.Builder(SubEventActivity.this, R.style.CustomAlertDialog);
+                ViewGroup viewGroup = findViewById(android.R.id.content);
+                View dialogView = LayoutInflater.from(v.getContext()).inflate(R.layout.dialog_layout, viewGroup, false);
+                dialogView.setMinimumWidth((int) (displayRectangle.width() * 1f));
+                dialogView.setMinimumHeight((int) (displayRectangle.height() * 1f));
+                builder.setView(dialogView);
+                final AlertDialog alertDialog = builder.create();
+                mLL = new ArrayList<>();
+                mProfile = new ArrayList<>();
+                mCall = new ArrayList<>();
+                mWhatsapp = new ArrayList<>();
+                mHeading = dialogView.findViewById(R.id.heading);
+                String s = "Event Heads";
+                mHeading.setText(s);
+
+                mLL.add((LinearLayout) dialogView.findViewById(R.id.one));
+                mLL.add((LinearLayout) dialogView.findViewById(R.id.two));
+                mLL.add((LinearLayout) dialogView.findViewById(R.id.three));
+
+                mProfile.add((ImageView) dialogView.findViewById(R.id.img1));
+                mProfile.add((ImageView) dialogView.findViewById(R.id.img2));
+                mProfile.add((ImageView) dialogView.findViewById(R.id.img3));
+
+                mWhatsapp.add((ImageView) dialogView.findViewById(R.id.whatsapp1));
+                mWhatsapp.add((ImageView) dialogView.findViewById(R.id.whatsapp2));
+                mWhatsapp.add((ImageView) dialogView.findViewById(R.id.whatsapp3));
+
+                mCall.add((ImageView) dialogView.findViewById(R.id.call1));
+                mCall.add((ImageView) dialogView.findViewById(R.id.call2));
+                mCall.add((ImageView) dialogView.findViewById(R.id.call3));
+
+                mDivider1 = dialogView.findViewById(R.id.divider1);
+                mDivider2 = dialogView.findViewById(R.id.divider2);
+
+                mLL.get(0).setVisibility(View.VISIBLE);
+                mLL.get(1).setVisibility(View.VISIBLE);
+
+                mDivider1.setVisibility(View.VISIBLE);
+                alertDialog.show();
+            }
+        });
+
+        mAboutLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Rect displayRectangle = new Rect();
+                Window window = SubEventActivity.this.getWindow();
+                window.getDecorView().getWindowVisibleDisplayFrame(displayRectangle);
+                final AlertDialog.Builder builder = new AlertDialog.Builder(SubEventActivity.this, R.style.CustomAlertDialog);
+                ViewGroup viewGroup = findViewById(android.R.id.content);
+                View dialogView = LayoutInflater.from(v.getContext()).inflate(R.layout.dialog_layout, viewGroup, false);
+                dialogView.setMinimumWidth((int) (displayRectangle.width() * 1f));
+                dialogView.setMinimumHeight((int) (displayRectangle.height() * 1f));
+                builder.setView(dialogView);
+                final AlertDialog alertDialog = builder.create();
+                mAbout = dialogView.findViewById(R.id.about_data);
+                mAbout.setVisibility(View.VISIBLE);
+                mHeading = dialogView.findViewById(R.id.heading);
+                String s = "About the event";
+                mHeading.setText(s);
+                alertDialog.show();
+            }
+        });
 
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onBackPressed() {
-        if(bottomSheetOpen){
+        if (bottomSheetOpen) {
             hideBottomSheet();
             bottomSheetOpen = false;
-        }else{
+        } else {
             finishAfterTransition();
         }
     }
@@ -93,7 +220,7 @@ public class SubEventActivity extends AppCompatActivity {
             public void onGenerated(@Nullable Palette palette) {
                 Palette.Swatch textSwatch = palette.getDarkMutedSwatch();
 
-                if(textSwatch!=null) {
+                if (textSwatch != null) {
                     toolbarIconColor = textSwatch.getTitleTextColor();
                     toolbar.setTitleTextColor(textSwatch.getTitleTextColor());
                 }
@@ -106,48 +233,57 @@ public class SubEventActivity extends AppCompatActivity {
         iv = findViewById(R.id.transition_img);
         frag_frame = findViewById(R.id.fragment_layout);
         toolbar = findViewById(R.id.toolbar);
+        mHeadLayout = findViewById(R.id.head_layout);
+        mAboutLayout = findViewById(R.id.about_layout);
+        mFab = findViewById(R.id.fab);
+        isOpen = false;
+        fadeIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+        fadeOut = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out);
     }
 
-    ArrayList<SubEventsModel> getData(){
+    ArrayList<SubEventsModel> getData() {
         String subEventsName[][] = Constants.SubEventsList;
-        for(int i = 0; i<subEventsName[mainEventPosition].length; i++){
+        for (int i = 0; i < subEventsName[mainEventPosition].length; i++) {
             data.add(new SubEventsModel(subEventsName[mainEventPosition][i]));
         }
         return data;
     }
+
     public void showBottomSheet() {
 
         EventBottomSheet bottomSheet = new EventBottomSheet();
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.setCustomAnimations(R.anim.slide_in_bottom,R.anim.no_anim);
-        transaction.add(R.id.fragment_layout,bottomSheet);
+        transaction.setCustomAnimations(R.anim.slide_in_bottom, R.anim.no_anim);
+        transaction.add(R.id.fragment_layout, bottomSheet);
         transaction.commit();
 
         //Change toolbar
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_cancel);
         toolbar.setBackgroundColor(Color.BLACK);
     }
-    private void hideBottomSheet(){
+
+    private void hideBottomSheet() {
         Fragment f = getSupportFragmentManager().findFragmentById(R.id.fragment_layout);
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.setCustomAnimations(R.anim.no_anim,R.anim.slide_out_bottom);
+        transaction.setCustomAnimations(R.anim.no_anim, R.anim.slide_out_bottom);
         transaction.remove(f).commit();
 
         //change toolbar
-        Drawable backArrow =  getResources().getDrawable(R.drawable.ic_arrow_back);
+        Drawable backArrow = getResources().getDrawable(R.drawable.ic_arrow_back);
         backArrow.setColorFilter(toolbarIconColor, PorterDuff.Mode.SRC_ATOP);
         getSupportActionBar().setHomeAsUpIndicator(backArrow);
         toolbar.setBackgroundColor(getResources().getColor(R.color.transparent));
     }
+
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()== android.R.id.home){
-            if(bottomSheetOpen){
+        if (item.getItemId() == android.R.id.home) {
+            if (bottomSheetOpen) {
                 hideBottomSheet();
                 bottomSheetOpen = false;
 
-            }else {
+            } else {
                 //go to EventsActivity with transition
                 finishAfterTransition();
             }
@@ -408,7 +544,6 @@ public class SubEventActivity extends AppCompatActivity {
             case "ScrapStar":
                 position = 82;
                 break;
-
 
 
         }
