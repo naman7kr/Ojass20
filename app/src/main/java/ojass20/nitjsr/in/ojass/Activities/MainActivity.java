01ee5f8943b5bf51d87a63e3a23a91c212241919
Harshit <harshitgupta12527@gmail.com>
2020-02-01 00:52:33 +0530

    Release bug rectified

--- app/src/main/java/ojass20/nitjsr/in/ojass/Activities/MainActivity.java
+++ app/src/main/java/ojass20/nitjsr/in/ojass/Activities/MainActivity.java
@@ -28,7 +28,6 @@ import android.view.animation.TranslateAnimation;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
 import android.widget.Scroller;
 import android.widget.TextView;
 import android.widget.Toast;
