ed110e6f46519b149c2cb63026c19dde1c7a8a0e
Naman <naman7kr@gmail.com>
2020-02-01 14:40:01 +0530

    Revert "Release bug rectified"
    This reverts commit 01ee5f89

--- app/src/main/java/ojass20/nitjsr/in/ojass/Activities/MainActivity.java
+++ app/src/main/java/ojass20/nitjsr/in/ojass/Activities/MainActivity.java
@@ -29,6 +29,7 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.Scroller;
 import android.widget.TextView;
 import android.widget.Toast;
